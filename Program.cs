150076548d5bb98e33efd6cc760375cb8440eaea
Zakaria Farhati <zakariafarhati18@gmail.com>
2020-01-11 19:18:35 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myAge;
+            //Console.WriteLine("What's your year now");
+            myAge = Convert.ToDateTime(Console.ReadLine());
+            int year = Convert.ToInt32(DateTime.Now.Subtract(myAge).TotalDays) / 360;
+            Console.WriteLine("Your age is " + year + " years old");
+            Console.ReadKey();
+        }
+    }
+}
